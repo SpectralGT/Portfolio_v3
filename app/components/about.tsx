2ff2c9cae658d6b6a5df29726b3db37fbc807229
SpectralGT <atharvsingh1396@gmail.com>
2026-01-03 18:59:54 -0500

    copy writting

---
+++ app/components/about.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <main>
+     <span>I'm Atharv - a Full Stack Developer crafting fast, scalable, and immersive digital experiences that merge creativity with engineering precision.</span>
+     <br></br>
+     <span> specialize in developing Saa platforms, AI-driven products, and interactive 3D web experiences using technologies like Next.js, Node.js, and Three.js.</span>
+    
+    <span> Years of Experience</span>
+    <span> Projects Completed</span>
+    <span> 4+</span>
+    <span> 30+</span>
+    </main>
+  );
+}
---
+++ app/components/hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <main>
+     <span>Hi! I'm Atharv</span>
+     <br></br>
+     <span>Full-stack Developer</span>
+    </main>
+  );
+}
