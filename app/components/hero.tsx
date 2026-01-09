6fea889384c936b3730f1e08533b2dc622618409
SpectralGT <atharvsingh1396@gmail.com>
2026-01-08 20:17:34 -0500

    updated font

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -2,9 +2,9 @@ export default function Hero() {
   return (
     <div className="h-screen bg-red-700">
       <div className="w-full h-full flex flex-col justify-center items-center">
-        <span className="text-xl">Hi! I&apos;m Atharv</span>
+        <span className="text-4xl archivo leading-tight">HI! I&apos;M ATHARV</span>
         <br></br>
-        <span>Full-stack Developer</span>
+        <span className="text-9xl archivo-black leading-tight">FULL-STACK DEV</span>
       </div>
     </div>
   );
