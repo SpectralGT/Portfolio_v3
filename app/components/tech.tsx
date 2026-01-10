2ad1faf123318940e0de0d7dd7cfe9f4737cce25
SpectralGT <atharvsingh1396@gmail.com>
2026-01-10 15:58:47 -0500

    added tech stack page

--- app/components/tech.tsx
+++ app/components/tech.tsx
@@ -1,11 +1,81 @@
+import "./tech.css";
+
 export default function Tech() {
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center pb-10">
-      <div className="text-2xl md:text-5xl text-center archivo-black w-[80vw]">My Technology Arsenal</div>
-      <div>
-        <div> Language & Frameworks</div>
+    <div className="h-screen w-screen pt-30 pb-10 px-10">
+      <div className="mb-10 w-screen text-2xl md:text-4xl text-center archivo-black">My Technology Arsenal</div>
+      <div className="techgrid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+        <div>
+          <div>Language & Frameworks</div>
+          <div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+          </div>
+        </div>
+        <div>
+          <div>Language & Frameworks</div>
+          <div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+          </div>
+        </div>
+        <div>
+          <div>Language & Frameworks</div>
+          <div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+          </div>
+        </div>
+        <div>
+          <div>Language & Frameworks</div>
+          <div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+          </div>
+        </div>
+        <div>
+          <div>Language & Frameworks</div>
+          <div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+          </div>
+        </div>
+        <div>
+          <div>Language & Frameworks</div>
+          <div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+            <div>Reactjs</div>
+          </div>
+        </div>
+
+
       </div>
-      
+
     </div>
   );
 }
