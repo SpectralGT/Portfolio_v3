497f71ecc978f28efd60ad06c00ad6188f6c738a
SpectralGT <atharvsingh1396@gmail.com>
2026-01-10 18:10:47 -0500

    added footer

--- app/components/tech.tsx
+++ app/components/tech.tsx
@@ -2,7 +2,7 @@ import "./tech.css";
 
 export default function Tech() {
   return (
-    <div className="h-screen w-screen pt-30 pb-10 px-10">
+    <div className="min-h-screen w-screen pt-30 pb-10 px-10">
       <div className="mb-10 w-screen text-2xl md:text-4xl text-center archivo-black">My Technology Arsenal</div>
       <div className="techgrid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import Hero from "./components/hero";
 import Header from "./components/header";
 import About from "./components/about";
 import Tech from "./components/tech";
+import Footer from "./components/footer";
 export default function Home() {
 
   return (
-    <main>
+    <main className="overflow-x-hidden overflow-y-hidden [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
       <Header/>
       <Hero/>
       <About/>
       <Tech/>
+      <Footer/>
     </main>
   );
 }
