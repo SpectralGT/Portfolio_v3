666ca077ce834df81e7fbf09b05b89fd8371f45d
SpectralGT <atharvsingh1396@gmail.com>
2026-01-11 23:52:29 -0500

    changed footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="h-screen w-screen flex flex-col justify-end bg-(--foreground)">
-        <div className="h-fit mt-auto archivo w-screen text-center text-[24vw] leading-[0.68] text-(--background)">ATHARV</div>
+        <div className="h-fit mt-auto archivo w-screen text-center text-[24vw] leading-[0.68] text-(--background)">ATHARV SINGH</div>
     </div>
   );
 }
